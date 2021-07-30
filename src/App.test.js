e298c1eb24a8c29b08d88261546ca7f0d0ddad13
Tolulope <toluls@ymail.com>
2021-07-30 12:03:49 -0600

    initial setup and configuration

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Grand Fashion text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const brandText = screen.getByText(/Grand Fashion/i);
+  expect(brandText).toBeInTheDocument();
 });
