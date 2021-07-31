22a304641e7579519417134235dff0549e22f673
Tolulope <toluls@ymail.com>
2021-07-31 17:25:26 -0600

    add top slider

--- src/Components/Layout/AppLayout.js
+++ src/Components/Layout/AppLayout.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import NavBar from './NavBar';
+import TopSlider from '../UI/TopSlider';
 
 const AppLayout = props => {
   return (
     <Fragment>
       <Header />
       <NavBar />
+      <TopSlider />
       <main>{props.children}</main>
       <Footer />
     </Fragment>
---
+++ src/Components/UI/TopSlider.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import classes from './TopSlider.module.scss';
+import image1 from '../../assets/img/slider_1.jpg';
+import image2 from '../../assets/img/slider_2.jpg';
+import image3 from '../../assets/img/slider_3.jpg';
+
+
+const TopSlider = () => {
+
+  return (
+    <div className={classes.topSlider}>
+      <AliceCarousel autoPlay autoPlayInterval="5000" autoPlayStrategy="action" disableButtonsControls infinite>
+        <img src={image1} className={classes.topSliderImg} alt="slider 1" />
+        <img src={image2} className={classes.topSliderImg} alt="slider 2" />
+        <img src={image3} className={classes.topSliderImg} alt="slider 3" />
+      </AliceCarousel>
+    </div>
+  );
+}
+
+export default TopSlider;
+
