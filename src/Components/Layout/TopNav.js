6d6b3363a6cc74eb088abd4aec5bb2a5b13353c3
Tolulope <toluls@ymail.com>
2021-08-01 14:10:35 -0600

    add menu icon and text for large screens

--- src/Components/Layout/TopNav.js
+++ src/Components/Layout/TopNav.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './TopNav.module.scss';
+import { MenuOpenIcon } from '../UI/Icons';
 
 const NavBar = () => {
   return (
     <div className={classes.topnav}>
+      <div className={classes.topnav__item}>
+        <span className={classes.topnav__icon}><MenuOpenIcon /></span>
+         All
+      </div>
       <Link to="/products" className={classes.topnav__item}>Today's Deals</Link>
       <Link to="/wish-list" className={classes.topnav__item}>Wish List</Link>
       <Link to="/customer-service" className={classes.topnav__item}>Help</Link>
