2b25ac9757821391b3858bbc871cdc59a741694a
Tolulope <toluls@ymail.com>
2021-08-03 00:05:09 -0600

    improved category section logic to handle unexpected paths

--- src/Components/Sections/ProductCategory.js
+++ src/Components/Sections/ProductCategory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProductDisplay from '../Products/ProductDisplay';
 import BannerDisplay from '../UI/BannerDisplay';
@@ -8,22 +8,30 @@ import classes from './ProductCategory.module.scss';
 
 const ProductCategory = () => {
   const params = useParams();
-  const category = params.productCategory;
-  const image = `/img/${category}.jpg`;
-
-  
+  const history = useHistory();
+  const urlCategory = params.productCategory;
+  const image = `/img/${urlCategory}.jpg`;
 
   const isLoading = useSelector(state => state.products.isLoading);
   const storeProducts = useSelector(state => state.products.products);
+  const products = storeProducts.filter(product => product.category.includes(urlCategory));
 
-  const products = storeProducts.filter(product => product.category.includes(category)) || [];
+  const appCategories = ['electronics', 'fashion', 'beauty', 'phones', 'computing'];
+  const isCategory = appCategories.includes(urlCategory)
 
-  return (
-    <div className={classes.category}>
-      <BannerDisplay image={image} />
-      <ProductDisplay isLoading={isLoading} products={products} adImage={displayAd1} heading={category.toUpperCase()} />
-    </div>
-  );
+
+  if (isCategory) {
+    return (
+      <div className={classes.category}>
+        <BannerDisplay image={image} />
+        <ProductDisplay isLoading={isLoading} products={products} adImage={displayAd1} heading={urlCategory.toUpperCase()} />
+      </div>
+    );
+  }
+  else {
+    history.replace('/products');
+    return null;
+  }
 }
 
 export default ProductCategory;
