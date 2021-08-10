dc43bb7b88e81adb32d5dfe17a8bd4910979d79e
Tolulope <toluls@ymail.com>
2021-08-09 18:10:10 -0600

    updated README with more details and screenshot

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Grand Fashion text', () => {
-  render(<App />);
-  const brandText = screen.getByText(/Grand Fashion/i);
-  expect(brandText).toBeInTheDocument();
-});
